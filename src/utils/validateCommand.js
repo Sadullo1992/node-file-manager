25af8f867ee55a3435601e88e6f0694dc07701fe
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-15 02:19:47 +0500

    fix: enable .exit cmd

--- src/utils/validateCommand.js
+++ src/utils/validateCommand.js
@@ -2,6 +2,8 @@ import { COMMANDS } from "../constants.js";
 import { InvalidInputError } from "./InvalidInputError.js";
 
 const validateCommand = async (cmd, args) => {
+  if (cmd === ".exit") return;
+
   const command = COMMANDS[cmd];
   if (!command) throw new InvalidInputError("Unknown Operation");
 
